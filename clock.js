147b2bd534a26fdbc677b7cbb36a57cfc2d5e150
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-01-22 22:37:11 +0100

    compiled assets

--- clock.js
+++ clock.js
@@ -1,80 +1,80 @@
-(function ($) {
-
-	if (typeof STUDIP !== 'object') {
-		throw 'No Stud.IP environment';
-	}
-
-	if (!$ || !$.fn || !$.fn.jquery || ($.fn.jquery < '1.4.3')) {
-		throw 'Invalid jQuery version, required version is 1.4.3';
-	}
-
-	var Clock = {
-
-		element: null,
-		format: 'H:i:s',
-		offset: 0,
-
-		initialize: function (id) {
-			var metadata = $(id).data() || {};
-
-			Clock.element = $(id);
-			Clock.format = metadata.format || Clock.format;
-			Clock.adjust(metadata.timestamp);
-			
-			setInterval(Clock.display, 100);
-		},
-		adjust: function (timestamp) {
-			Clock.offset = timestamp ? timestamp - (new Date()).getTime() : 0;
-		},
-		display: function () {
-			var now = (new Date()).getTime() + Clock.offset;
-			Clock.element.text(Clock.date(now));
-		},
-
-		// Quick port of PHP's date() function to JS, FAR from complete
-		days: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-		mapping: {d: 'getDate', H: 'getHours', i: 'getMinutes', s: 'getSeconds'},
-		date: function (timestamp) {
-			if (typeof timestamp !== 'object') {
-				timestamp = new Date(timestamp);
-			}
-
-			return Clock.format.replace(/[dHilmsY]/g, function (token) {
-				if (Clock.mapping[token]) {
-					return pad(timestamp[Clock.mapping[token]]());
-				}
-				switch (token) {
-					case 'l': return Clock.days[timestamp.getDay()].toLocaleString();
-					case 'm': return pad(timestamp.getMonth() + 1);
-					case 'Y': return (timestamp.getYear() % 100) + 2000;
-					default: return token;
-				}
-			});
-		}
-
-	};
-
-	// Pads a given string to a length of 2
-	function pad(what) {
-		return ('0' + what).slice(-2);
-	}
-
-	$(document).ready(function () {
-		// Uni Augsburg: move clock to infobox
-		if ($('.clockip').is('.uni-augsburg')) {
-			var infobox_clock = $('&nbsp;<b>Aktuelle Serverzeit:</b><br>');
-			$('.clockip').addClass('sidebar').appendTo(infobox_clock);
-			$('.infoboxrahmen').append(infobox_clock);
-		}
-
-		Clock.initialize('.clockip');
-	});
-
-	// Make clock object globally accessible
-	STUDIP.CLOCK = Clock;
-
-    // Enable periodic updates
-    STUDIP.jsupdate_enable = true;
-    
-}(window.jQuery));
-
+(function($, STUDIP) {
+  var Clock, date, pad;
+  if (!($ && $.fn && $.fn.jquery && $.fn.jquery >= '1.4.3')) {
+    throw 'Invalid jQuery version, required version is 1.4.3';
+  }
+  if (typeof STUDIP !== 'object') throw 'No Stud.IP environment';
+  date = function(timestamp, format) {
+    var days, mapping;
+    if (format == null) format = 'H:i:s';
+    if (typeof timestamp !== 'object') timestamp = new Date(timestamp);
+    days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+    mapping = {
+      d: 'getDate',
+      H: 'getHours',
+      i: 'getMinutes',
+      s: 'getSeconds'
+    };
+    return format.replace(/[dHilmsY]/g, function(token) {
+      var method;
+      method = mapping[token];
+      if (typeof method !== 'undefined') return pad(timestamp[method]());
+      switch (token) {
+        case 'l':
+          return days[timestamp.getDay()].toLocaleString();
+        case 'm':
+          return pad(timestamp.getMonth() + 1);
+        case 'Y':
+          return (timestamp.getYear() % 100) + 2000;
+        default:
+          return token;
+      }
+    });
+  };
+  pad = function(what) {
+    return ('0' + what).slice(-2);
+  };
+  Clock = function(element, offset, format) {
+    var adjust, display, interval, metadata, _ref,
+      _this = this;
+    if (element == null) element = '.clockip';
+    if (offset == null) offset = 0;
+    if (format == null) format = 'H:i:s';
+    adjust = function(timestamp) {
+      return offset = timestamp ? timestamp - (new Date()).getTime() : 0;
+    };
+    display = function() {
+      var now;
+      now = (new Date()).getTime();
+      return element.text(date(now + offset, format));
+    };
+    this.start = function() {
+      var interval;
+      _this.stop();
+      return interval = setInterval(display, 100);
+    };
+    this.stop = function() {
+      var interval;
+      if (interval !== null) clearInterval(interval);
+      return interval = null;
+    };
+    this.element = element = $(element);
+    interval = null;
+    metadata = (_ref = element.data()) != null ? _ref : {};
+    if (typeof metadata.format !== 'undefined') format = metadata.format;
+    if (typeof metadata.timestamp !== 'undefined') {
+      return adjust(metadata.timestamp);
+    }
+  };
+  return $(function() {
+    var clock, infobox_clock;
+    clock = new Clock;
+    if (clock.element.hasClass('uni-augsburg')) {
+      infobox_clock = $("<b>Aktuelle Serverzeit:</b><br>");
+      clock.element.addClass('sidebar');
+      clock.element.appendTo(infobox_clock);
+      $('.infoboxrahmen').append(infobox_clock);
+    }
+    return clock.start();
+  });
+})(jQuery, STUDIP);
